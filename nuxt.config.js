77e6aaf069de66e8aefc0dc2b7684f279ab2bb2b
Alex Welcing <alexwelcing@gmail.com>
2021-02-25 14:26:15 -0500

    switch to comma even though feels wrong

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  module.exports = {
+  module,exports = {
     modules: ['nuxt-ghost'],
 
     ghost: {
